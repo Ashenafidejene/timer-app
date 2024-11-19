7a932844111abac5819f14c3d2b89698a6fd28f0
Ashenafidejene <ashenafidejene75@gmail.com>
2024-11-19 20:16:53 +0300

    error handling
    i add a restricted mode on the input to prevent letter and negative number

--- Timer/src/renderer/src/components/InputFiled.jsx
+++ Timer/src/renderer/src/components/InputFiled.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
 export default function InputFiled({label,value,onChange,placeholder}) {
-
+  const handleInputChange = (e) => {
+    const inputvalue = e.target.value;
+    if (/^\d+$/.test(inputvalue)) { // Correct capitalization
+      onChange(e);
+    }
+  };
   return (
     <div className='text-3xl'>
       <label
       className='text-stone-300'
       >{label}:</label>
-      <input type='number' value={value} onChange={onChange} placeholder={placeholder}
+      <input type='number' value={value} onChange={handleInputChange} placeholder={placeholder}
       className='w-20 bg-transparent text-blue-400' ></input>
     </div>
   )
